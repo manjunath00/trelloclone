aa21c81dbeddf51beb20ea5e7303ffe228357b7d
manjunath00 <kadrimanjunath56@gmail.com>
2020-04-19 23:09:37 +0530

    added create card functionality

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,32 +1,49 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import urls from "../../apis/getUrls";
 import trello from "../../apis/trelloapi";
 import Card from "../Card/Card";
+import Toggler from "../common/Toggler";
 
 function List(props) {
   const [listId, setListId] = useState(props.id);
   const [cards, setCards] = useState([]);
+  const [newCard, setNewCard] = useState(false);
 
   useEffect(() => {
     getAllCards(listId);
-  }, [listId]);
+  }, [listId, newCard]);
 
   const getAllCards = async (listId) => {
     const response = await trello.get(urls.getAllCards(listId));
-    console.log("Response cards ", response);
+    // console.log("Response cards ", response);
     setCards(response.data);
     return response.data;
   };
 
+  const addANewCard = async (cardName) => {
+    // console.log(cardName)
+    const body = {
+      idList: listId,
+      name: cardName
+    }
+    const response = await trello.post(urls.postACard(), body);
+    setNewCard(true);
+    // console.log(response)
+  }
+
   return (
-    <div className="list-container">
+    <div>
       <div>
-        <h5>{props.card.name}</h5>
+        <h5>{props.list.name}</h5>
       </div>
-      {cards.map((card) => (
-        <Card card={card} key={card.id} />
-      ))}
+      <div>
+        {cards.map((card) => (
+          <Card card={card} key={card.id} />
+        ))}
+      </div>
+
+      <Toggler desc={"Create a Card"} onSubmit={addANewCard}/>
     </div>
   );
 }
