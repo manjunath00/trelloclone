668d3aaaf3e7cf9a7ff441f924dfca72e0d94684
manjunath00 <kadrimanjunath56@gmail.com>
2020-04-19 23:11:05 +0530

    added functionality to create a new list

--- src/components/List/Lists.js
+++ src/components/List/Lists.js
@@ -1,28 +1,52 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import urls from "../../apis/getUrls";
 import trello from "../../apis/trelloapi";
 import List from "./List";
+import Toggler from "../common/Toggler";
 
 function Lists(props) {
-  const boardId = props.match.params.id;
+  // console.log(props);
+  const boardId = props.props.match.params.id;
   const [id, setId] = useState(boardId);
   const [lists, setLists] = useState([]);
+  const [isUpdated, setIsUpdated] = useState(null);
 
   useEffect(() => {
     getAllLists(id);
-  }, [id]);
+  }, [id, isUpdated]);
 
   const getAllLists = async (boardId) => {
-    const response = await trello.get(urls.getAllLists(boardId));
-    setLists(response.data);
+    try {
+      const response = await trello.get(urls.getAllLists(boardId));
+      setLists(response.data);
+      setIsUpdated(true);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const createAList = async (listName) => {
+    console.log(listName);
+    try {
+      const body = {
+        idBoard: boardId,
+        name: listName,
+      };
+      const response = await trello.post(urls.postAList(), body);
+      // console.log(response)
+      setIsUpdated(false);
+    } catch (err) {
+      console.log(err.message);
+    }
   };
 
   return (
     <div className="list-container flex">
       {lists.map((list) => (
-        <List card={list} key={list.id} id={list.id} />
+        <List list={list} key={list.id} id={list.id} />
       ))}
+      <Toggler desc="Create New List" onSubmit={createAList} />
     </div>
   );
 }
