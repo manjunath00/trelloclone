d7bf62e1a6c59888ba97460360a265609d1123f9
manjunath00 <kadrimanjunath56@gmail.com>
2020-04-28 23:03:58 +0530

    action types refactored

--- src/actions/actiontypes.js
+++ src/actions/actiontypes.js
@@ -1,18 +1,30 @@
-/* GET Requests */
+/* boards */
 export const GET_ALL_BOARDS = "GET_ALL_BOARDS";
+export const CREATE_A_BOARD = "CREATE_A_BOARD";
+export const UPDATE_A_BOARD = "UPDATE_A_BOARD";
+export const DELETE_A_BOARD = "DELETE_A_BOARD";
+
+/* lists */
 export const GET_ALL_LISTS = "GET_ALL_LISTS";
-export const GET_ALL_CARDS = "GET_ALL_CARDS";
-export const GET_ALL_CHECKLISTS = "GET_ALL_CHECKLISTS";
-export const GET_ALL_CHECKLISTITEMS = "GET_ALL_CHECKLISTITEMS";
-
-/* POST Requests */
 export const ADD_A_LIST = "ADD_A_LIST";
-export const ADD_A_CARD = "ADD_A_CARD";
-export const ADD_A_CHECKLIST = "ADD_A_CHECKLIST";
-export const ADD_A_CHECKLISTITEM = "ADD_A_CHECKLISTITEM";
-
-/* PUT Requests */
 export const UPDATE_A_LIST = "UPDATE_A_LIST";
+export const DELETE_A_LIST = "DELETE_A_LIST";
+
+/* cards */
+export const GET_ALL_CARDS = "GET_ALL_CARDS";
+export const ADD_A_CARD = "ADD_A_CARD";
 export const UPDATE_A_CARD = "UPDATE_A_CARD";
+export const DELETE_A_CARD = "DELETE_A_CARD";
+
+/* checklists */
+export const GET_ALL_CHECKLISTS = "GET_ALL_CHECKLISTS";
+export const ADD_A_CHECKLIST = "ADD_A_CHECKLIST";
 export const UPDATE_A_CHECKLIST = "UPDATE_A_CHECKLIST";
+export const DELETE_A_CHECKLIST = "DELETE_A_CHECKLIST";
+
+/* checklist-items */
+export const GET_ALL_CHECKLISTITEMS = "GET_ALL_CHECKLISTITEMS";
+export const ADD_A_CHECKLISTITEM = "ADD_A_CHECKLISTITEM";
 export const UPDATE_A_CHECKLISTITEM = "UPDATE_A_CHECKLISTITEM";
+export const TOGGLE_A_CHECKLISTITEM = "TOGGLE_A_CHECKLISTITEM"; 
+export const DELETE_A_CHECKLISTITEM = "DELETE_A_CHECKLISTITEM";
